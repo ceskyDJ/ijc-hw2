125e8fafdfaa5919ce9536acaa15c1a23b48b0e9
ceskyDJ <48548230+ceskyDJ@users.noreply.github.com>
2021-04-12 23:06:02 +0200

    Better code structure in tail

--- src/tail.c
+++ src/tail.c
@@ -13,12 +13,95 @@
 // Maximum row size in memory (MAX_ROW_LENGTH +1 => '\n', +1 => '\0')
 #define ROW_SIZE MAX_ROW_LENGTH + 2
 
+// Buffer for storing cached rows
+typedef struct buffer {
+    unsigned size;
+    char *(data)[];
+} buffer_t;
+
+/**
+ * Allocates memory for a new buffer and initialize the structure
+ * @param buffer_size Size of the buffer (number of stored rows)
+ * @return Pointer to prepared buffer
+ */
+buffer_t *alloc_buffer(unsigned buffer_size) {
+    buffer_t *buffer;
+    if ((buffer = malloc(sizeof(buffer_t) + buffer_size * sizeof(char *))) == NULL) {
+        fprintf(stderr, "Nepodařilo se alokovat paměť pro buffer\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < (int)buffer_size; i++) {
+        if ((buffer->data[i] = malloc(ROW_SIZE)) == NULL) {
+            fprintf(stderr, "Nepodařilo se alokovat paměť pro řádek bufferu\n");
+            exit(1);
+        }
+
+        memset(buffer->data[i], '\0', ROW_SIZE);
+    }
+
+    buffer->size = buffer_size;
+
+    return buffer;
+}
+
+/**
+ * Shifts buffer items to create free space at the end of the buffer
+ * @param buffer Buffer to modify
+ */
+void shift_buffer_items(buffer_t *buffer) {
+    for (int j = 0; j < (int)buffer->size - 1; j++) {
+        memcpy(buffer->data[j], buffer->data[j + 1], ROW_SIZE);
+    }
+}
+
+/**
+ * Adds a new row at the end of the buffer
+ * @param buffer Buffer to modify
+ * @param read_line Row to insert
+ */
+void push_row_to_buffer(buffer_t *buffer, char *read_line) {
+    // Shift buffer rows
+    shift_buffer_items(buffer);
+
+    // Put the content to the end row of the buffer
+    memcpy(buffer->data[buffer->size - 1], read_line, ROW_SIZE);
+}
+
+/**
+ * Writes buffer content
+ * @param buffer Buffer to write
+ */
+void write_buffer(buffer_t *buffer) {
+    for (int j = 0; j < (int)buffer->size; j++) {
+        printf("%s", buffer->data[j]);
+    }
+}
+
+/**
+ * Deallocates buffer allocated in memory
+ * @param buffer Buffer to deallocate
+ */
+void deallocate_buffer(buffer_t *buffer) {
+    for (int j = 0; j < (int)buffer->size; j++) {
+        free(buffer->data[j]);
+    }
+
+    free(buffer);
+}
+
+/**
+ * Program for writing last (or first) N lines of the file
+ * @param argc Number of input arguments
+ * @param argv Array of input arguments. Format: [0] => "-n", [1] => N, [2] => [file]. All arguments are optional.
+ * @return Exit code. 0 if success, 1 if something failed
+ */
 int main(int argc, char *argv[]) {
     // File to read from
     FILE *input_file = stdin;
     bool default_file = true;
     // Row offset from the beginning/end of the file (how many rows will be read)
-    int row_offset = 10;
+    unsigned row_offset = 10;
     // Row offset direction - true => from end of file; false => from the beginning of the file
     bool end_offset = true;
 
@@ -71,21 +154,10 @@ int main(int argc, char *argv[]) {
     }
 
     // Prepare buffer with initial allocated memory
-    char **buffer;
+    // Buffer is not needed if the opposite direction mode is activated (end_offset == true)
+    buffer_t *buffer;
     if (end_offset) {
-        if ((buffer = malloc(row_offset * sizeof(char *))) == NULL) {
-            fprintf(stderr, "Nepodařilo se alokovat paměť pro buffer\n");
-            exit(1);
-        }
-
-        for (int i = 0; i < row_offset; i++) {
-            if ((buffer[i] = malloc(ROW_SIZE)) == NULL) {
-                fprintf(stderr, "Nepodařilo se alokovat paměť pro řádek bufferu\n");
-                exit(1);
-            }
-
-            memset(buffer[i], '\0', ROW_SIZE);
-        }
+        buffer = alloc_buffer(row_offset);
     }
 
     // Load rows into buffer or write rows
@@ -120,15 +192,10 @@ int main(int argc, char *argv[]) {
             ignore_next = true;
         }
 
+        // Process input
         if (end_offset) {
-            // Shift buffer rows
-            for (int j = 0; j < row_offset - 1; j++) {
-                memcpy(buffer[j], buffer[j + 1], ROW_SIZE);
-            }
-
-            // Put the content to the end row of the buffer
-            memcpy(buffer[row_offset - 1], read_line, ROW_SIZE);
-        } else if (i >= row_offset - 1) {
+            push_row_to_buffer(buffer, read_line);
+        } else if (i >= (int)row_offset - 1) {
             printf("%s", read_line);
         }
 
@@ -140,14 +207,10 @@ int main(int argc, char *argv[]) {
         fclose(input_file);
     }
 
+    // Write buffer content to stdout and deallocate buffer if it has been used
     if (end_offset) {
-        // Write output of the buffer
-        for (int j = 0; j < row_offset; j++) {
-            printf("%s", buffer[j]);
-            free(buffer[j]);
-        }
-
-        free(buffer);
+        write_buffer(buffer);
+        deallocate_buffer(buffer);
     }
 
     return 0;
